b87b1084382e4695db5f436d316054f6221d2da7
Aaron Stopher <22336995+aastopher@users.noreply.github.com>
2023-08-14 23:53:30 -0600

    prep release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stack",
-    version="0.0.1",
+    version="0.1.0",
     author="Aaron Stopher",
     packages=setuptools.find_packages(include=["stack"]),
     description="Stack is designed to stack functions into a register, this registered stack is designed to be consumed by a stack of modular utilities.",
